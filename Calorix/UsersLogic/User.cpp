ffc3f4910796d2a79489c1e6a9039eb2b43114ad
yoanaml <yoana.mladenova00@gmail.com>
2026-06-21 00:46:08 +0300

    User implamantation new logic

--- Calorix/UsersLogic/User.cpp
+++ Calorix/UsersLogic/User.cpp
@@ -1,48 +1,35 @@
 #include "User.h"
 #include <stdexcept>
 
-int User::idGenerator = 0;
+int User::s8IdGenerator = 0;
 
 User::User(const std::string& username, const std::string& password, UserProfile profile)
-	: userId(idGenerator++), username(username), password(password), profile(profile)
+    : s8UserId(s8IdGenerator++), username(username), password(password), profile(profile)
 {
-	if (username.empty()) {
-		throw std::invalid_argument("Sorry, username cannot be empty. Think of a cool name!");
-	}
-	if (password.empty()) {
-		throw std::invalid_argument("Sorry, password cannot be empty. It is not secure for you!");
-	}
-}
-
-User::User(int id,
-    const std::string& username,
-    const std::string& password,
-    UserProfile profile)
-    : userId(id),
-    username(username),
-    password(password),
-    profile(profile)
-{
-    if (username.empty()) {
-        throw std::invalid_argument(
-            "Sorry, username cannot be empty. Think of a cool name!");
+    if (username.empty())
+    {
+        throw std::invalid_argument("Sorry, username cannot be empty. Think of a cool name!");
     }
-
-    if (password.empty()) {
-        throw std::invalid_argument(
-            "Sorry, password cannot be empty. It is not secure for you!");
+    if (password.empty())
+    {
+        throw std::invalid_argument("Sorry, password cannot be empty. It is not secure for you!");
     }
 }
-std::string User::getUsername() const
+
+User::User(int s8Id, const std::string& username, const std::string& password, UserProfile profile)
+    : s8UserId(s8Id), username(username), password(password), profile(profile)
 {
-	return username;
+    if (username.empty())
+    {
+        throw std::invalid_argument("Sorry, username cannot be empty. Think of a cool name!");
+    }
+    if (password.empty())
+    {
+        throw std::invalid_argument("Sorry, password cannot be empty. It is not secure for you!");
+    }
 }
 
-std::string User::getPassword() const
-{
-	return password;
-}
-
-int User::getId() const {
-	return userId;
-}
+std::string        User::getUsername() const { return username; }
+std::string        User::getPassword() const { return password; }
+int                User::getId()       const { return s8UserId; }
+const UserProfile& User::getProfile()  const { return profile; }
--- Calorix/UsersLogic/User.h
+++ Calorix/UsersLogic/User.h
@@ -1,20 +1,25 @@
-#pragma once
 #include "UserProfile.h"
 #include <string>
-class User {
-private:
-	int userId;
-	std::string username;
-	std::string password;
-	UserProfile profile;
-public:
-	static int idGenerator;
-	User(const std::string& username, const std::string& password, UserProfile profile);
-	User(int id, const std::string& username, const std::string& password, UserProfile profile);
-	std::string getUsername() const;
-	std::string getPassword() const;
-	int getId() const;
 
-	virtual ~User() = default;
-	virtual void help() const = 0;
+class User
+{
+private:
+    int         s8UserId;
+    std::string username;
+    std::string password;
+    UserProfile profile;
+
+public:
+    static int s8IdGenerator;
+
+    User(const std::string& username, const std::string& password, UserProfile profile);
+    User(int s8Id, const std::string& username, const std::string& password, UserProfile profile);
+
+    std::string        getUsername() const;
+    std::string        getPassword() const;
+    int                getId()       const;
+    const UserProfile& getProfile()  const;
+
+    virtual ~User() = default;
+    virtual void help() const = 0;
 };
