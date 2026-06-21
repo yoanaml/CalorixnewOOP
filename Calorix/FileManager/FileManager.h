ef3c7dd94891f834066e3b864cd6651842abc478
yoanaml <yoana.mladenova00@gmail.com>
2026-06-21 03:47:32 +0300

    File manager  declaration

---
+++ Calorix/FileManager/FileManager.h
@@ -0,0 +1,11 @@
+#include <string>
+
+class Calorix;
+
+class FileManager
+{
+public:
+    static void save(const Calorix& system, const std::string& filename);
+    static void load(Calorix& system, const std::string& filename);
+};
+
